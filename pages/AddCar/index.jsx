7a10599fef851f96c223e89ce5958c661b91269a
Faris-Hmd <bluocat4@gmail.com>
2023-01-07 10:45:12 +0200

    improv res

--- pages/AddCar/index.jsx
+++ pages/AddCar/index.jsx
@@ -13,111 +13,120 @@ const AddCar = () => {
     console.log(formData);
   }, [formData]);
   return (
-    <form className={styles.form}>
-      <div className={styles.inputCon}>
-        <label htmlFor="fName">الاسم الاول</label>
+    <>
+      <div className="label" style={{ direction: "rtl" }}>
+        استمارة الافراج المؤقت
+      </div>
+      <form className={styles.form}>
+        <div className={styles.inputCon}>
+          <label htmlFor="fName">الاسم الاول</label>
+          <input
+            type="text"
+            name="fName"
+            placeholder="الاسم الاول"
+            onChange={handleChage}
+          />
+        </div>
+        <div className={styles.inputCon}>
+          <label htmlFor="sName">الاسم الثاني</label>
+          <input
+            type="text"
+            name="sName"
+            placeholder="الاسم الثاني"
+            onChange={handleChage}
+          />
+        </div>
+        <div className={styles.inputCon}>
+          <label htmlFor="sName">الاسم الثاني</label>{" "}
+          <input
+            type="text"
+            name="tName"
+            placeholder="الاسم الثالث"
+            onChange={handleChage}
+          />
+        </div>
+        <div className={styles.inputCon}>
+          <label htmlFor="sName">الاسم الرابع</label>{" "}
+          <input
+            type="text"
+            name="foName"
+            placeholder="الاسم الرابع"
+            onChange={handleChage}
+          />
+        </div>{" "}
+        <div className={styles.inputCon}>
+          <label htmlFor="sName">رقم الجواز</label>
+          <input
+            type="text"
+            name="passport"
+            placeholder="رقم الجواز"
+            onChange={handleChage}
+          />
+        </div>
+        <div className={styles.inputCon}>
+          <label htmlFor="sName">رقم الدفتر</label>{" "}
+          <input
+            type="text"
+            name="bookNum"
+            placeholder="رقم الدفتر"
+            onChange={handleChage}
+          />
+        </div>{" "}
+        <div className={styles.inputCon}>
+          <label htmlFor="sName">رقم الهاتف</label>
+          <input
+            type="number"
+            name="phones"
+            placeholder="رقم الهاتف"
+            onChange={handleChage}
+          />
+        </div>
+        <div className={styles.inputCon}>
+          <label htmlFor="sName">رقم الشاسيه</label>
+          <input
+            type="text"
+            name="chaseNum"
+            placeholder="رقم الشاسيه"
+            onChange={handleChage}
+          />
+        </div>
+        <div className={styles.inputCon}>
+          <label htmlFor="sName">نوع المركبة</label>
+          <input
+            type="text"
+            name="carType"
+            placeholder="نوع المركبة"
+            onChange={handleChage}
+          />
+        </div>
+        <div className={styles.inputCon}>
+          <label htmlFor="bookType">سياحي</label>
+          <select name="bookType" id="bookType" onChange={handleChage}>
+            <option value={true}>نعم</option>
+            <option value={false}>لا</option>
+          </select>{" "}
+          <label htmlFor="enteringType">نوع الدخول</label>
+          <select name="enteringType" id="enterType" onChange={handleChage}>
+            <option value={"جديد"}>جديد</option>
+            <option value={"مكرر"}>مكرر</option>
+          </select>
+        </div>
+        <label htmlFor="bookDate">تاريخ الدفتر</label>
         <input
-          type="text"
-          name="fName"
-          placeholder="الاسم الاول"
+          type="date"
+          name="bookDate"
+          id="bookDate"
           onChange={handleChage}
         />
-      </div>
-      <div className={styles.inputCon}>
-        <label htmlFor="sName">الاسم الثاني</label>
+        <label htmlFor="enteringDate">تاريخ الدخول</label>
         <input
-          type="text"
-          name="sName"
-          placeholder="الاسم الثاني"
+          type="date"
+          name="enteringDate"
+          id="enteringDate"
           onChange={handleChage}
         />
-      </div>
-      <div className={styles.inputCon}>
-        <label htmlFor="sName">الاسم الثاني</label>{" "}
-        <input
-          type="text"
-          name="tName"
-          placeholder="الاسم الثالث"
-          onChange={handleChage}
-        />
-      </div>
-      <div className={styles.inputCon}>
-        <label htmlFor="sName">الاسم الرابع</label>{" "}
-        <input
-          type="text"
-          name="foName"
-          placeholder="الاسم الرابع"
-          onChange={handleChage}
-        />
-      </div>
-      <div className={styles.inputCon}>
-        <label htmlFor="sName">رقم الدفتر</label>{" "}
-        <input
-          type="text"
-          name="bookNum"
-          placeholder="رقم الدفتر"
-          onChange={handleChage}
-        />
-      </div>
-      <div className={styles.inputCon}>
-        <label htmlFor="sName">رقم الشاسيه</label>
-        <input
-          type="text"
-          name="chaseNum"
-          placeholder="رقم الشاسيه"
-          onChange={handleChage}
-        />
-      </div>
-      <div className={styles.inputCon}>
-        <label htmlFor="sName">رقم الجواز</label>
-        <input
-          type="text"
-          name="passport"
-          placeholder="رقم الجواز"
-          onChange={handleChage}
-        />
-      </div>
-      <div className={styles.inputCon}>
-        <label htmlFor="sName">رقم الهاتف</label>
-        <input
-          type="number"
-          name="phones"
-          placeholder="رقم الهاتف"
-          onChange={handleChage}
-        />
-      </div>
-      <div className={styles.inputCon}>
-        <label htmlFor="sName">نوع المركبة</label>
-        <input
-          type="text"
-          name="carType"
-          placeholder="نوع المركبة"
-          onChange={handleChage}
-        />
-      </div>
-      <div className={styles.inputCon}>
-        <label htmlFor="bookType">سياحي</label>
-        <select name="bookType" id="bookType" onChange={handleChage}>
-          <option value={true}>نعم</option>
-          <option value={false}>لا</option>
-        </select>{" "}
-        <label htmlFor="enteringType">نوع الدخول</label>
-        <select name="enteringType" id="enterType" onChange={handleChage}>
-          <option value={"جديد"}>جديد</option>
-          <option value={"مكرر"}>مكرر</option>
-        </select>
-      </div>
-
-      <label htmlFor="bookDate">تاريخ الدفتر</label>
-      <input type="date" name="bookDate" id="bookDate" onChange={handleChage} />
-      <label htmlFor="enteringDate">تاريخ الدخول</label>
-      <input
-        type="date"
-        name="enteringDate"
-        id="enteringDate"
-        onChange={handleChage}
-      />
-    </form>
+      </form>
+    </>
   );
 };
 export default AddCar;
