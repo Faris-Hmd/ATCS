8524c18549957a1296a3c8efb7ab26001de254e9
Faris-Hmd <bluocat4@gmail.com>
2023-01-09 23:19:49 +0200

    setCar

--- pages/api/addCars.js
+++ pages/api/addCars.js
@@ -7,12 +7,8 @@ export default async function handler(req, res) {
   const car = req.body;
   console.log(car);
 
-<<<<<<< HEAD
-  await setDoc(doc(db, "cars", car.bookNum), { ...car });
-  console.log(car);
-=======
   await setDoc(doc(db, "cars", car.bookNum), car);
+  console.log(car);
 
->>>>>>> 0b029b3a9e7ae84f763a70b24694abf627fd749b
   res.status(200).json(true);
 }
