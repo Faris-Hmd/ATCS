ce789d7b187e30ff07429ef22f6d16e031a003b6
Faris-Hmd <bluocat4@gmail.com>
2023-02-03 23:08:18 +0200

    hide report

--- pages/CarDetail/[bookNum].jsx
+++ pages/CarDetail/[bookNum].jsx
@@ -187,7 +187,7 @@ const CarDetail = () => {
           </Tab>
         </Tabs>
         {!isEditing && (
-          <div className="">
+          <div className="hidden">
             <LeftingReportToPrint ref={leftReportRef} value={customer} />
             <ExtentionReportToPrint ref={exReportRef} value={customer} />
           </div>
